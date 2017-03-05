19db08067cc4ac3320a3cda8fe92289d466d62ab
BraveApple <1404767913@qq.com>
2017-03-05 08:09:26 +0800

    2017-03-04-19-33-1488627238

--- src/caffe/layers/memory_data_layer.cpp
+++ src/caffe/layers/memory_data_layer.cpp
@@ -50,18 +50,20 @@ void MemoryDataLayer<Dtype>::AddDatumVector(const vector<Datum>& datum_vector) {
   this->data_transformer_->Transform(datum_vector, &added_data_);
   // Copy Labels
   Dtype* top_label = this->added_label_.mutable_cpu_data();
+  // for (int item_id = 0; item_id < num; ++item_id) {
+  //   top_label[item_id] = datum_vector[item_id].label();
+  // }
 
   // multi-label
   for (int item_id = 0; item_id < num; ++item_id) {
     for(int label_id = 0; label_id < this->dim_label_; ++label_id) {
       top_label[item_id * this->dim_label_ + label_id] = datum_vector[item_id].label(label_id)
     }
-    top_label[item_id] = datum_vector[item_id].label();
   }
   // num_images == batch_size_
-  Dtype* top_data = added_data_.mutable_cpu_data();
+  Dtype* top_data = this->added_data_.mutable_cpu_data();
   Reset(top_data, top_label, num);
-  has_new_data_ = true;
+  this->has_new_data_ = true;
 }
 
 #ifdef USE_OPENCV
@@ -72,21 +74,30 @@ void MemoryDataLayer<Dtype>::AddMatVector(const vector<cv::Mat>& mat_vector,
   CHECK(!has_new_data_) <<
       "Can't add mat until current data has been consumed.";
   CHECK_GT(num, 0) << "There is no mat to add";
-  CHECK_EQ(num % batch_size_, 0) <<
+  CHECK_EQ(num % this->batch_size_, 0) <<
       "The added data must be a multiple of the batch size.";
-  added_data_.Reshape(num, channels_, height_, width_);
-  added_label_.Reshape(num, 1, 1, 1);
+  added_data_.Reshape(num, this->channels_, this->height_, this->width_);
+  // added_label_.Reshape(num, 1, 1, 1);
+  added_label_.Reshape(num, this->dim_label_, 1, 1);
   // Apply data transformations (mirror, scale, crop...)
   this->data_transformer_->Transform(mat_vector, &added_data_);
   // Copy Labels
-  Dtype* top_label = added_label_.mutable_cpu_data();
+  Dtype* top_label = this->added_label_.mutable_cpu_data();
+  // for (int item_id = 0; item_id < num; ++item_id) {
+  //   top_label[item_id] = labels[item_id];
+  // }
+
+  // multi-label
   for (int item_id = 0; item_id < num; ++item_id) {
-    top_label[item_id] = labels[item_id];
+    for(int label_id = 0; label_id < this->dim_label_; ++label_id) {
+      top_label[item_id * this->dim_label_ + label_id] = datum_vector[item_id].label(label_id)
+    }
   }
+
   // num_images == batch_size_
-  Dtype* top_data = added_data_.mutable_cpu_data();
+  Dtype* top_data = this->added_data_.mutable_cpu_data();
   Reset(top_data, top_label, num);
-  has_new_data_ = true;
+  this->has_new_data_ = true;
 }
 #endif  // USE_OPENCV
 
@@ -94,38 +105,44 @@ template <typename Dtype>
 void MemoryDataLayer<Dtype>::Reset(Dtype* data, Dtype* labels, int n) {
   CHECK(data);
   CHECK(labels);
-  CHECK_EQ(n % batch_size_, 0) << "n must be a multiple of batch size";
+  CHECK_EQ(n % this->batch_size_, 0) << "n must be a multiple of batch size";
   // Warn with transformation parameters since a memory array is meant to
   // be generic and no transformations are done with Reset().
   if (this->layer_param_.has_transform_param()) {
     LOG(WARNING) << this->type() << " does not transform array data on Reset()";
   }
-  data_ = data;
-  labels_ = labels;
-  n_ = n;
-  pos_ = 0;
+  this->data_ = data;
+  this->labels_ = labels;
+  this->n_ = n;
+  this->pos_ = 0;
 }
 
 template <typename Dtype>
 void MemoryDataLayer<Dtype>::set_batch_size(int new_size) {
   CHECK(!has_new_data_) <<
       "Can't change batch_size until current data has been consumed.";
-  batch_size_ = new_size;
-  added_data_.Reshape(batch_size_, channels_, height_, width_);
-  added_label_.Reshape(batch_size_, 1, 1, 1);
+  this->batch_size_ = new_size;
+  this->added_data_.Reshape(this->batch_size_, this->channels_, this->height_, this->width_);
+  //added_label_.Reshape(batch_size_, 1, 1, 1);
+  // multi-label
+  this->added_label_.Reshape(this->batch_size_, this->dim_label_, 1, 1);
 }
 
 template <typename Dtype>
 void MemoryDataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  CHECK(data_) << "MemoryDataLayer needs to be initialized by calling Reset";
-  top[0]->Reshape(batch_size_, channels_, height_, width_);
-  top[1]->Reshape(batch_size_, 1, 1, 1);
-  top[0]->set_cpu_data(data_ + pos_ * size_);
-  top[1]->set_cpu_data(labels_ + pos_);
-  pos_ = (pos_ + batch_size_) % n_;
-  if (pos_ == 0)
-    has_new_data_ = false;
+  CHECK(this->data_) << "MemoryDataLayer needs to be initialized by calling Reset";
+  top[0]->Reshape(this->batch_size_, this->channels_, this->height_, this->width_);
+  //top[1]->Reshape(batch_size_, 1, 1, 1);
+  // multi-label
+  top[1]->Reshape(this->batch_size_, this->dim_label_, 1, 1);
+  top[0]->set_cpu_data(this->data_ + this->pos_ * this->size_);
+  //top[1]->set_cpu_data(labels_ + pos_);
+  // muti-label
+  top[1]->set_cpu_data(this->labels_ * this->dim_label_ + pos_);
+  this->pos_ = (this->pos_ + this->batch_size_) % this->n_;
+  if (this->pos_ == 0)
+    this->has_new_data_ = false;
 }
 
 INSTANTIATE_CLASS(MemoryDataLayer);
